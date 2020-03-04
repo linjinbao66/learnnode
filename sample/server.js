2697c581f82ec005c25bd5dca7d9215ab0a3ec2f
linjb <linjb@fline88.com>
2020-03-04 09:57:31 +0800

    update

--- sample/server.js
+++ sample/server.js
@@ -1,6 +1,10 @@
-var fs = require('fs');
-var path = require('path');
-var http = require('http');
+/**
+ * 模块引入
+ */
+var fs = require('fs'),
+    path = require('path'),
+    http = require('http');
+
 
 var MIME = {
     '.css':'text/css',
@@ -8,15 +12,15 @@ var MIME = {
 }
 
 /**
- * 
+ * @description 文件拼接函数
  * @param {路径名称} pathnames 
  * @param {回调函数} callback 
  */
 function combileFiles(pathnames, callback){
     var output = [];
-
     /**
      * 闭包函数
+     * 从0开始，到pathnames.len截至 处理url中出现的文件数量
      */
     (function next(i, len){
         if(i<len){
@@ -24,14 +28,14 @@ function combileFiles(pathnames, callback){
                 if(err){
                     callback(err)
                 }else{
-                    output.push(data);
-                    next(i+1, len);
+                    output.push(data);  //读到output中
+                    next(i+1, len);     //递归调用
                 }
             });
         }else{
-            callback(null, Buffer.concat(output));
+            callback(null, Buffer.concat(output));  //Buffer.concat(output)最终会赋值到data
         }
-    })(0, pathnames.len);
+    })(0, pathnames.length);
 
 }
 
@@ -42,7 +46,8 @@ function main(argv){
 
     http.createServer(function(request, response){
         var urlInfo = parseURL(root, request.url);
-
+        console.log('urlInfo = ' + urlInfo.pathnames)
+        console.log('urlInfo = ' + urlInfo.mime)
         combileFiles(urlInfo.pathnames, function(err, data){
             if(err){
                 response.writeHead(404);
@@ -59,20 +64,29 @@ function main(argv){
     
 }
 
+/**
+ * @description url解析
+ * @param {根目录} root 
+ * @param {*} url 
+ * @returns mime 文件的扩展名
+ * @returns pathnames 路径名称 pathnames = servercopy.js,server.js
+ */
 function parseURL(root, url) {
     var base, pathnames, parts;
 
     if (url.indexOf('??') === -1) {
-        url = url.replace('/', '/??');
+        url = url.replace('/', '/??');  //替换
     }
 
-    parts = url.split('??');
+    parts = url.split('??');    //切割
     base = parts[0];
     pathnames = parts[1].split(',').map(function (value) {
         return path.join(root, base, value);
     });
+    console.log('pathnames = ' + pathnames)
 
     return {
+        //path.extname返回path路径文件扩展名
         mime: MIME[path.extname(pathnames[0])] || 'text/plain',
         pathnames: pathnames
     };
@@ -82,3 +96,16 @@ function parseURL(root, url) {
  * 入口
  */
 main(process.argv.slice(2));
+
+/**
+ * 回调的理解：
+ * 1. 程序进入main方法
+ * ...
+ * 2. combileFiles(urlInfo.pathnames, function(err, data) 这一行调用combileFiles函数，并传入两个参数，
+ * 第二个参数是一个函数类型，并且该函数类型需要两个形参err,和data
+ * 3. combileFiles(pathnames, callback) 函数需要两个参数，第二个是一个回调函数，即callback是调用combileFiles
+ * 时调用者提供的，此处调用者是http.createServer，提供的callback是function(err, data)
+ * 4. callback(null, Buffer.concat(output));参数null赋值给function(err, data)中的err,Buffer.concat(output)
+ * 赋值给function(err, data)中的data
+ * 5. 至此http.createServer取到了需要返回的数据放在data中，response.end(data);返回data数据
+ */
